105da6ad53608b754f257585acb77a892c0f9e1d
Alexis Rosovsky <alex.roso@gmail.com>
2025-05-04 01:12:59 +0200

    Update aider.sh to use paid gemini instead of free gemini

--- aider.sh
+++ aider.sh
@@ -1,1 +1,1 @@
-aider --edit-format udiff-simple --read CONVENTIONS.md --editor-edit-format udiff-simple
+aider --model gemini --edit-format udiff-simple --read CONVENTIONS.md --editor-edit-format udiff-simple
